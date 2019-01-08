89b74c761ade7c28a7f97ca82cc88830df02d8c5
gem install sass <gem install sass>
2019-01-08 20:31:58 +0800

    onetijiao

--- src/views/women/index.js
+++ src/views/women/index.js
@@ -1,9 +1,29 @@
 import React,{Component} from 'react'
-
+import {getimg} from './model.js'
 class Girls extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			looplist: []
+		}
+	}
 
+	componentDidMount() {
+			getimg().then(res => {
+				this.setState({
+					looplist: res
+				})
+			})
+	}
 	render(){
-		return <div>women</div>
+		return <div id="women">
+			<div>
+			{
+				this.state.looplist.length !== 0 ?
+				<img src={this.state.looplist[0].main_image} className="banner" alt=""/>:null 
+			}
+			</div>	
+		</div>
 	}
 }
 
