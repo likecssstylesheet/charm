dd4fa9cae02127bb38844d713c262b7060e4c40e
pendulum <1764912028@qq.com>
2019-01-09 09:45:35 +0800

    pendulum

---
+++ src/views/produceDetail/index.js
@@ -0,0 +1,41 @@
+import React,{Component} from 'react'
+import axios from 'axios'
+import './index.scss'
+
+
+class Produce extends Component{
+
+	render(){
+		return (
+			<div id="detail">
+				
+			</div>
+			)
+	}
+	componentDidMount(){
+		axios({
+			url:`http://www.mei.com/appapi/product/getAppProductDetailUrl/v3?${this.props.match.params.data}`
+					
+			}).then((res)=>{
+				// console.log(res.data)
+				axios({
+					url:`http://www.mei.com/appapi/product/getProductPrice/v3?productId=${res.data.productId}&userLevel=2&type=0`
+				}).then(res=>{
+					console.log(res.data)
+				})
+
+				axios({
+					url:`http://www.mei.com/appapi/product/detail/v3?categoryId=${res.data.eventId}&productId=${res.data.productId}&platform_code=H5&timestamp=${time()}&summary=ec7fd7ec49470eae3632aa3b865a4eee`
+				}).then(res=>{
+					console.log(res.data)
+				})
+			})
+	}
+}
+
+function time(){
+	var date = new Date();
+	return date.getTime();
+}
+
+export default Produce
