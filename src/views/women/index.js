0115b423d9fd24f86b25d2209845ec5e9742de78
gem install sass <gem install sass>
2019-01-09 15:43:38 +0800

    two

--- src/views/women/index.js
+++ src/views/women/index.js
@@ -1,28 +1,83 @@
 import React,{Component} from 'react'
-import {getimg} from './model.js'
+import {getimg,getul,getlist} from './model.js'
+import {NavLink} from 'react-router-dom'
+import Footer from '../../components/footer'
 class Girls extends Component{
 	constructor(props){
 		super(props)
 		this.state = {
-			looplist: []
+			looplist: [],
+			ullist: [],
+			navlist:[]
 		}
 	}
 
-	componentDidMount() {
+	componentWillMount() {
 			getimg().then(res => {
 				this.setState({
 					looplist: res
 				})
 			})
-	}
+
+			getul().then(res => {
+				this.setState({
+					ullist: res
+				})
+			})
+
+			getlist().then(res => {
+				this.setState({
+					navlist: res
+				})
+			})
+		}
 	render(){
 		return <div id="women">
-			<div>
-			{
-				this.state.looplist.length !== 0 ?
-				<img src={this.state.looplist[0].main_image} className="banner" alt=""/>:null 
-			}
-			</div>	
+				{
+					this.state.looplist.map(item => {
+						return	<div key={item.id} className="first">
+									<img src={item.main_image} className="banner" alt=""/>
+									<div className="info">
+										<h3>{item.main_title}</h3>
+										<p>{item.sub_title}</p>
+										<p>{item.description}</p>
+									</div>
+									 <ul className="nav">
+									 {
+										this.state.ullist.map(item => {
+											return	<li key={item.categoryTwoId}>
+												<NavLink to="">	
+													<img src={item.categoryImgStr} alt=""/>
+												</NavLink>
+											</li>
+										})				
+									 }
+									</ul>									
+								</div>
+					})
+
+					
+				}
+
+				<ul className="data_info">
+					 {
+						this.state.navlist.map(item => {
+							return	<li key={item.eventId}>
+								<NavLink to="">	
+									<img src={item.imageUrl} alt=""/>
+								</NavLink>
+									<div>
+										<p>{item.englishName}</p>
+										<p>{item.chineseName}</p>
+										<p>{item.discountText}</p>
+									</div>
+							</li>
+						})				
+					 }
+				</ul>
+					 
+				
+			<Footer></Footer>
 		</div>
 	}
 }
--- src/views/women/model.js
+++ src/views/women/model.js
@@ -1,11 +1,30 @@
-import axios from 'axios'
+import axios from 'axios' 
 import './index.scss'
 function getimg() {
 	return axios({
 		url: 'http://www.mei.com/appapi/home/mktBannerApp/v3?silo_id=2013000100000000001&platform_code=PLATEFORM_H5'
 	}).then(res => {
-		console.log(res.data.banners)
+		// console.log(res.data.banners)
 		return res.data.banners
 	})
 }
-export {getimg}
+
+function getul() {
+	return axios({
+		url: 'http://www.mei.com/appapi/cms/cmsDetail/v3?silo=2013000100000000001&ids=2120000100000000276&timestamp=1546998371964&summary=e60787909845a82b89e0ddaa57c800a9&platform_code=H5'
+	}).then(res => {
+		// console.log(res.data.resultList[0].data)
+		return res.data.resultList[0].data
+	})
+}
+
+function getlist() {
+	return axios({
+		url: 'http://www.mei.com/appapi/silo/eventForH5?categoryId=women&pageIndex=1&timestamp=1546998371823&summary=c18bd9dce7d2c8d583252932e735e4d2&platform_code=H5'
+	}).then(res => {
+		// console.log(res.data.resultList[0].data)
+		return res.data.eventList
+	})
+}
+
+export {getimg,getul,getlist}
