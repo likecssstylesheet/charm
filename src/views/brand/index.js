79a1771ff42c16881e33561d704d704014dc2049
liry <1031401101@qq.com>
2019-01-10 07:56:13 +0800

    b

--- src/views/brand/index.js
+++ src/views/brand/index.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import getBrandInfo from './model.js'
+import {getBrandInfo} from './model.js'
 class Brand extends Component {
 	constructor(props) {
 		super(props)
