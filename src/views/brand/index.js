dc307fc3310ba1b7e5e5fe1f1755a1b2d3d1ea05
liuhongbin1 <1352228477@qq.com>
2019-01-10 10:29:17 +0800

    333

--- src/views/brand/index.js
+++ src/views/brand/index.js
@@ -1,9 +1,8 @@
 import React,{Component} from 'react'
-<<<<<<< HEAD
+
 import {getBrandInfo }from './model.js'
-=======
-import {getBrandInfo} from './model.js'
->>>>>>> b5ab5bd1f5385f115c73a77f443fc01a26e48eaa
+
+
 class Brand extends Component {
 	constructor(props) {
 		super(props)
