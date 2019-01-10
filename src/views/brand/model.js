ea7d1b2838e9af27ddd6488db6f7a43e1e86b310
liuhongbin1 <1352228477@qq.com>
2019-01-10 08:59:02 +0800

    swiper

--- src/views/brand/model.js
+++ src/views/brand/model.js
@@ -1,5 +1,5 @@
 import axios from 'axios' 
-import qs from 'qs'
+
 function getBrandInfo() {
 	return axios({
 		url: 'http://www.mei.com/appapi/brand/brandInfo/v3'
--- src/views/lifestyle/index.js
+++ src/views/lifestyle/index.js
@@ -1,80 +1,35 @@
 import React,{Component} from 'react'
 import { Carousel, WingBlank } from 'antd-mobile';
 import axios from 'axios'
+import './index.scss'
 
-// class Lifestyle extends React.Component {
-//   state = {
-//     data: [],
-//     imgHeight: 176,
-//   }
-//   componentDidMount() {
-//     // simulate img loading
-     
-// 	return axios.get('http://www.mei.com/appapi/home/mktBannerApp/v3?silo_id=2013000100000000004&platform_code=PLATEFORM_H5')
-// 	.then(res=>{
-// 		console.log(res.data.banners)
-// 		this.setState({data:res.data.banners})})
-
-//   }
-//   render() {
-//     return (
-//     <div className="life_swipe">
-//       <WingBlank style={{width:'100%'}}>
-//         <Carousel key={this.state.data.length}
-//           autoplay={true}
-//           infinite
-//           beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-//           afterChange={index => console.log('slide to', index)}
-//         >
-//           {this.state.data.map(val => (
-//               <img  key={val.id}
-//                 src={val.main_image}
-//                 alt=""
-//                 style={{ width: '100%', verticalAlign: 'top' }}
-//                 onLoad={() => {
-//                   // fire window resize event to change height
-//                   window.dispatchEvent(new Event('resize'));
-//                   this.setState({ imgHeight: 'auto' });
-//                 }}
-//               />
-           
-//           ))}
-//         </Carousel>
-//       </WingBlank>
-//       </div>
-//     );
-//   }
-// }
 class Lifestyle extends React.Component {
   state = {
-    data: ['1', '2', '3'],
+    data: [],
     imgHeight: 176,
   }
   componentDidMount() {
     // simulate img loading
-    setTimeout(() => {
-      this.setState({
-        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-      });
-    }, 100);
+     
+	return axios.get('http://www.mei.com/appapi/home/mktBannerApp/v3?silo_id=2013000100000000004&platform_code=PLATEFORM_H5')
+	.then(res=>{
+		console.log(res.data.banners)
+		this.setState({data:res.data.banners})})
+
   }
   render() {
     return (
-      <WingBlank>
-        <Carousel
-          autoplay={false}
+    <div className="life_swipe">
+      <WingBlank style={{width:'100%'}} >
+        <Carousel key={this.state.data.length}
+          autoplay={true}
           infinite
           beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
           afterChange={index => console.log('slide to', index)}
         >
           {this.state.data.map(val => (
-            <a
-              key={val}
-              href="http://www.alipay.com"
-              style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
-            >
-              <img
-                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+              <img  key={val.id}
+                src={val.main_image}
                 alt=""
                 style={{ width: '100%', verticalAlign: 'top' }}
                 onLoad={() => {
@@ -83,12 +38,14 @@ class Lifestyle extends React.Component {
                   this.setState({ imgHeight: 'auto' });
                 }}
               />
-            </a>
+           
           ))}
         </Carousel>
       </WingBlank>
+      </div>
     );
   }
 }
+
 
 export default Lifestyle
